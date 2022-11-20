8b416ac0b91c038b18f38fe886cf3b1ef017f372
danilsklyar <100023622+danilsklyar@users.noreply.github.com>
2022-11-20 12:45:09 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function() {
+
+    $('.header-slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+    $('.slider__items').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    })
+})
